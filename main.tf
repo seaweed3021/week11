70a1042feebb9a0f6f618b5ac2c3d6f87f85b720
Aneeta <84175569+seaweed3021@users.noreply.github.com>
2026-08-02 23:45:40 -0400

    Modify ingress rules in aws_security_group
    Updated ingress rules for the web security group to restrict access to a specific CIDR block.

--- main.tf
+++ main.tf
@@ -62,12 +62,12 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ingress {
+     from_port   = 8080
+     to_port     = 8080
+     protocol    = "tcp"
+     cidr_blocks = ["203.0.113.0/24"]
+   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
