4715d8729472e56fe2dd0b9bf3de40f15b181bdf
Aneeta <84175569+seaweed3021@users.noreply.github.com>
2026-08-03 00:00:27 -0400

    Update Terraform configuration for AWS instance

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     aws = {
@@ -13,10 +12,8 @@ terraform {
     }
   }
   required_version = ">= 1.1.0"
-
   cloud {
     organization = "REPLACE_ME"
-
     workspaces {
       name = "gh-actions-demo"
     }
@@ -31,17 +28,14 @@ resource "random_pet" "sg" {}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
 
@@ -59,26 +53,34 @@ resource "aws_instance" "web" {
   }
 
   user_data = <<-EOF
-              ...
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
               EOF
 }
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
-ingress {
-     from_port   = 8080
-     to_port     = 8080
-     protocol    = "tcp"
-     cidr_blocks = ["203.0.113.0/24"]
-   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["203.0.113.0/24"]
+  }
+
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-# tfsec:ignore:aws-ec2-no-public-egress-sgr -- outbound required for apt-get package installs
+  # tfsec:ignore:aws-ec2-no-public-egress-sgr -- outbound required for apt-get package installs
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
